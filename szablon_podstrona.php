1eda01028216e53f7f243b2f4aa012e7940660e9
Maro <rexex.webdev@gmail.com>
2021-01-31 18:04:41 +0100

    - subSite pt.1

---
+++ szablon_podstrona.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- DO WYKASOWANIA JAK BĘDZIE GOTOWE PONIŻEJ!!!! -->
+  <META NAME="robots" CONTENT="noindex,nofollow">
+  <!-- KONIEC: DO WYKASOWANIA JAK BĘDZIE GOTOWE!!!! -->
+  <title>Blog - Remonty Marcina</title>
+  <!-- favicon -->
+  <link rel="icon" href="img/icons/favicon/favicon.ico" type="image/x-icon" />
+  <link rel="shortcut icon" href="img/icons/favicon/favicon.ico" type="image/x-icon" />
+  <link rel="apple-touch-icon" sizes="180x180" href="img/icons/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="img/icons/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="img/icons/favicon/favicon-16x16.png">
+
+  <!-- main CSS -->
+  <link rel="stylesheet" href="_css/main.css" />
+  <!-- bootstrap CSS-->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <!-- <link rel="stylesheet" href="_css/bootstrap.min.css"/> -->
+
+  <!-- Google fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+  <!-- fancybox CSS -->
+  <link rel="stylesheet" href="_css/jquery.fancybox.min.css" />
+
+  <!-- JQuery, bootstrap JS -->
+  <script src="js/jquery-3.5.1.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+
+  <!-- hamburgersJS -->
+  <script defer src="js/hamMenu.js"></script>
+  <!-- fancybox options -->
+  <script defer src="js/jquery.fancybox.min.js"></script>
+  <script defer src="js/fancyboxOptions.js"></script>
+  <!-- smoothScrollAnchor -->
+  <script defer src="js/smoothScrollAnchor.js"></script>
+  <!-- Cookie alert -->
+  <script defer src="js/cookieAlert.js"></script>
+  <!-- FB likebox script -->
+  <div id="fb-root"></div>
+  <script async defer crossorigin="anonymous" src="https://connect.facebook.net/pl_PL/sdk.js#xfbml=1&version=v9.0" nonce="TzuYivcU"></script>
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-0855GS2N5K"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-0855GS2N5K');
+    gtag('config', 'AW-460448545');
+  </script>
+
+  <!-- Event snippet for Kontakt conversion page
+In your html page, add the snippet and call gtag_report_conversion when someone clicks on the chosen link or button. -->
+  <script>
+    function gtag_report_conversion(url) {
+      var callback = function() {
+        if (typeof(url) != 'undefined') {
+          window.location = url;
+        }
+      };
+      gtag('event', 'conversion', {
+        'send_to': 'AW-460448545/cVeECLmGr-0BEKHGx9sB',
+        'event_callback': callback
+      });
+      return false;
+    }
+  </script>
+</head>
+
+<body>
+  <div class="wrapper">
+    <div class="menuMobileContainer">
+      <div id="menuLogo">
+        <img src="img/jumbo/RMwhite.svg" alt="">
+      </div>
+      <ul>
+        <li><a href="index.php">Strona&nbsp;Główna</a></li>
+        <li><a href="#top2">Top</a></li>
+        <li><a href="#article">Artykuły</a></li>
+        <li><a href="#contact2">Kontakt</a></li>
+      </ul>
+    </div>
+    <div class="menuMainContainer LG-H-BE">
+      <ul>
+        <li><a href="index.php">Strona&nbsp;Główna</a></li>
+        <li><a href="#top2">Top</a></li>
+        <li><a href="#article">Artykuły</a></li>
+        <li><a href="#contact2">Kontakt</a></li>
+      </ul>
+    </div>
+    <header>
+      <div id="jumbo" class="cardContainer">
+        <div id="logo">
+          <img src="img/jumbo/RMblue.svg" />
+        </div>
+
+        <div class="numbers">
+          <div class="phoneIcon">
+            <a href="tel:512818102"><img src="img/icons/phoneNumber.svg" alt="phone number icon" />
+            </a>
+          </div>
+        </div>
+
+        <div id="menuHam" class="LG-H-AB">
+          <div class="hamburger hamburger--squeeze">
+            <div class="hamburger-box">
+              <div class="hamburger-inner"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <section>
+      <div id="welcome" class="cardContainer centeredContent">
+
+        <!-- ARTYKUŁ DO KOPIOWANIA PONIŻEJ-->
+        <div class="text_sub">
+          <div class="artilcle_img f-box">
+            <!-- do podmiany obrazek z katalogu "MARCIN" poniżej -->
+            <img src="marcin/obrazek1_C.jpg" alt="">
+          </div>
+          <h2>Nazwa Artykułu 1</h2>
+          <p>
+            Remonty Marcina to firma remontowo - wykończeniowa działająca na terenie miasta Poznania i okolic.
+            <br />
+            Praca wykończeniowe wnętrz to nasza pasja i specjalność.
+            Cenimy estetykę i gwarantujemy wysoką jakość wykonanych przez nas prac.
+            Oferujemy przygotowanie do zamieszkania domy jedno – i wielorodzinne.
+            Podejmujemy się remontów mieszkań w stanie deweloperskim, jak i już zamieszkanych.
+            <br />
+            Łączymy najnowsze trendy architektoniczne z pomysłami klienta.
+            Najważniejsze jest dla nas przede wszystkim satysfakcja klienta, dlatego remonty wykonujemy w ścisłej współpracy z nim.
+            Doradzamy sprawdzone rozwiązania.
+          </p>
+        </div>
+        <!-- KONIEC: ARTYKUŁ DO KOPIOWANIA -->
+
+        <!-- ARTYKUŁ DO KOPIOWANIA PONIŻEJ-->
+        <div class="text_sub">
+          <div class="artilcle_img f-box">
+            <!-- do podmiany obrazek z katalogu "MARCIN" poniżej -->
+            <img src="marcin/obrazek2_C.jpg" alt="">
+          </div>
+          <h2>Nazwa Artykułu 2</h2>
+          <p>
+            Remonty Marcina to firma remontowo - wykończeniowa działająca na terenie miasta Poznania i okolic.
+            <br />
+            Praca wykończeniowe wnętrz to nasza pasja i specjalność.
+            Cenimy estetykę i gwarantujemy wysoką jakość wykonanych przez nas prac.
+            Oferujemy przygotowanie do zamieszkania domy jedno – i wielorodzinne.
+            Podejmujemy się remontów mieszkań w stanie deweloperskim, jak i już zamieszkanych.
+            <br />
+            Łączymy najnowsze trendy architektoniczne z pomysłami klienta.
+            Najważniejsze jest dla nas przede wszystkim satysfakcja klienta, dlatego remonty wykonujemy w ścisłej współpracy z nim.
+            Doradzamy sprawdzone rozwiązania.
+          </p>
+        </div>
+        <!-- KONIEC: ARTYKUŁ DO KOPIOWANIA -->
+
+      </div>
+    </section>
+    <footer>
+      <div id="contact" class="cardContainer centeredContent">
+        <div id="footerBox" class="f-box">
+          <!-- form  -->
+          <?php
+            include 'php/mail_form.php';
+          ?>
+          <div id="form" class="footerEl footerBkg">
+            <p>Formularz kontaktowy</p>
+            <form class="" action="index.php" method="post">
+              <div class="form-group">
+                <input id="name_form" class="form-control" type="text" name="client_name" placeholder="imię" maxlength="50" required>
+              </div>
+              <div class="form-group">
+                <input id="email_form" class="form-control" type="email" name="client_email" placeholder="e-mail" required>
+              </div>
+              <div class="form-group">
+                <input id="phone_form" class="form-control" type="tel" name="client_tel" placeholder="numer telefonu &lpar;opcjonalnie&rpar;" minlength="9" maxlength="11">
+              </div>
+              <div class="form-group">
+                <textarea id="question_form" class="form-control" name="client_question" placeholder="zadaj pytanie lub opisz jaką pracę chcesz nam zlecić" rows="5" cols="10" maxlength="700" required></textarea>
+              </div>
+              <!-- <div class="form-group">
+                <label for="captcha_form">dodaj liczby:&nbsp; -->
+              <?php
+                  // include 'php/captcha.php';
+                ?>
+              <!-- </label>
+                <input id="captcha_form" class="form-control" type="number" name="captcha_sum" placeholder="wpisz wynik" required maxlength="2">
+              </div> -->
+              <div class="form-group custom-control custom-checkbox">
+                <input id="check_form" class="custom-control-input" type="checkbox" required>
+                <label class="custom-control-label" for="check_form">akceptuję <a href="#privacyPolicy" data-toggle="modal">politykę prywatności</a></label>
+              </div>
+              <button type="submit" class="btn btn-primary" name="submit">wyślij</button>
+              <button type="reset" class="btn btn-danger" value="wyczyść">wyczyść</button>
+            </form>
+            <!-- privacyPolicy Modal -->
+            <div class="modal" id="privacyPolicy">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h4 class="modal-title">Polityka prywatośći</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                  </div>
+                  <div class="modal-body">
+                    1. Administratorem Państwa danych osobowych jest REMONTY MARCINA MARCIN HOFFMANN </br>
+                    2. Poniżej znajdują się wyszczególnione cele przetwarzania danych osobowych wraz z podstawami prawnymi:</br>
+                    A. w celu podjęcia na Państwa żądanie działań przed zawarciem umowy na świadczenie usług zgodnie z zakresem naszej działalności przetwarzamy następujące dane osobowe:
+                    Imię i nazwisko, adres zamieszkania, nazwa firmy, siedziba firmy, adres do korespondencji, adres planowanych prac, adres e-mail, numer telefonu, NIP, REGON, informacje zawarte w rejestrach publicznych, numer rachunku bankowego,
+                    numer IP oraz inne informacje, które umieszczone zostały w dokumentacji, którą od Państwa otrzymaliśmy;</br>
+                    B. w celu zawarcia i realizacji umowy, czyli świadczenie usług przetwarzamy następujące dane osobowe:
+                    Imię i nazwisko, adres zamieszkania, nazwa firmy, siedziba firmy, adres do korespondencji, adres prac, adres email, numer telefonu, NIP, REGON, informacje zawarte w rejestrach publicznych, numer rachunku bankowego, numer IP oraz
+                    inne informacje, które umieszczone zostały w dokumentacji, którą od Państwa otrzymaliśmy; podstawą prawną takiego przetwarzania danych jest art. 6 ust. 1 lit. b RODO, który pozwala przetwarzać dane osobowe, jeżeli są one konieczne
+                    do wykonania umowy lub podjęcia czynności zmierzających do zawarcia umowy między nami.</br>
+                    C. w celu wykonania ciążących na nas obowiązków prawnych, np.: wystawienia faktury VAT i dokonania rozliczeń podatkowych, przetwarzamy następujące dane osobowe:
+                    Imię i nazwisko, nazwa firmy, adres zamieszkania, siedzibę firmy, adres do korespondencji, adres e-mail, NIP, REGON, informacje zawarte w rejestrach publicznych, numer rachunku bankowego.
+                    numer IP oraz inne informacje, które umieszczone zostały w dokumentacji, którą od Państwa otrzymaliśmy;
+                    podstawą prawną takiego przetwarzania danych jest art. 6 ust. 1 lit. b RODO, który pozwala przetwarzać dane
+                    osobowe, jeżeli są one konieczne do wykonania umowy.
+                    interesem Administratora jest posiadanie danych osobowych, które pozwolą ustalić, dochodzić lub bronić się przed</br>
+                    D. w celu administrowania stroną internetową (automatyczne zapisywanie poniższych danych w tzw. Logach serwera, przy każdorazowym korzystaniu z naszej strony www) możemy przetwarzać takie
+                    dane osobowe jak:</br>
+                    Adres IP, data i czas serwera, informacje o przeglądarce internetowej, informacje o systemie operacyjnym;</br>
+                    J. w celu marketingu bezpośredniego, przetwarzamy następujące dane
+                    Imię i nazwisko, nazwa firmy, adres e-mail, numer telefonu, adres do korespondencji, dane zawarte w rejestrach publicznych np. KRS, CEIDG;</br>
+                    JAK ZREALIZOWAĆ PRAWO DO WYCOFANIA ZGODY?</br>
+                    1. Jeżeli przetwarzanie danych osobowych odbywa się na podstawie Twojej zgody, w każdej chwili możesz tą zgodę cofnąć — wedle własnego uznania.</br>
+                    2. Jeżeli chciałbyś cofnąć zgodę na przetwarzanie danych osobowych, to w tym celu wystarczy wysłać list bezpośrednio do Administratora Danych Osobowych lub wysłać email z dopiskiem „RODO”.</br>
+                    CZY PROWADZIMY ZAAUTOMATYZOWANE PODEJMOWANIE DECYZJI I PROFILOWANIE?</br>
+                    Uprzejmie informujemy, że korzystamy z narzędzi zautomatyzowanego podejmowania decyzji, w tym w oparciu o profilowanie. Używamy plików cookies Facebooka, które pozwalają nam zbierać informacje o sposobie korzystania z witryny
+                    przez użytkownika w tym na profilowanie.
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">zamknij</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- END privacyPolicy Modal -->
+          </div>
+          <!-- END form -->
+          <div id="contact_details" class="footerEl footerBkg">
+            <div id="footerLogo">
+              <h2><img src="img/jumbo/RMblue.svg" alt="kontakt" /></h2>
+            </div>
+            <div class="phoneFooter">
+              <a href="tel:512818102"><img src="img/icons/phoneNumber.svg" alt="512818102 512 818 102" />
+              </a>
+            </div>
+            <!-- <div class="phoneFooter">
+            <a href="tel:514455311"><img src="img/icons/phoneNumber2.svg" alt="phone number icon" />
+            </a>
+          </div> -->
+            <div id="mailTo">
+              <a href="mailto:remontymarcina@gmail.com"><img src="img/icons/mailTo.svg" alt="remontymarcina@gmail.com" /></a>
+            </div>
+            <div id="fbRM">
+              <a href="https://www.facebook.com/RemontyMarcina/" target="_blank"><img src="img/icons/fbRemontyMarcina.svg" alt="Facebook Remonty Marcina" /></a>
+            </div>
+            <div id="regon">
+              <a href="https://prod.ceidg.gov.pl/CEIDG/CEIDG.Public.UI/SearchDetails.aspx?Id=738f4e29-60bd-4d0a-bf56-07862616fef4" target="_blank"><img src="img/icons/regon.svg" alt="regon" /></a>
+            </div>
+            <div id="nip">
+              <a href="https://prod.ceidg.gov.pl/CEIDG/CEIDG.Public.UI/SearchDetails.aspx?Id=738f4e29-60bd-4d0a-bf56-07862616fef4" target="_blank"><img src="img/icons/nip.svg" alt="NIP 7792355201" /></a>
+            </div>
+          </div>
+        </div>
+
+
+        <!-- fb_likebox -->
+        <div class="fb-like" data-href="https://www.facebook.com/RemontyMarcina/" data-width="" data-layout="button_count" data-action="like" data-size="large" data-share="false"></div>
+        <!-- Google map -->
+        <div id="myMap">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2434.9938017771433!2d16.850419389621585!3d52.38866268082674!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47044510d58cba21%3A0x929d2ae33f6d84fd!2sREMONTY%20MARCINA%20MALOWANIE%20I%20SZPACHLOWANIE%20NATRYSKOWE!5e0!3m2!1spl!2spl!4v1609604686047!5m2!1spl!2spl"
+            width="100%" height="100%" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        </div>
+      </div>
+    </footer>
+  </div>
+</body>
+
+</html>
